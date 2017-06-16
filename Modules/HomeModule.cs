f85f62317480744100e694872f5b1aaa5e81907e
guy <luckythelich@hotmail.com>
2017-06-16 14:16:16 -0700

    add bandform view and route. add success view.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -2,7 +2,8 @@ using Nancy;
 using System.Collections.Generic;
 using System;
 using Nancy.ViewEngines.Razor;
-
+using Venue_Object;
+using Band_Object;
 
 namespace BandTracker_Modules
 {
@@ -13,6 +14,26 @@ namespace BandTracker_Modules
 
       Get["/"] = _ => {
         return View["index.cshtml"];
+      };
+
+      Get["/bands"] = _ => {
+        List<Band> AllBands = Band.GetAll();
+        return View["bands.cshtml", AllBands];
+      };
+
+      Get["/venues"] = _ => {
+        List<Venue> AllVenues = Venue.GetAll();
+        return View["venues.cshtml", AllVenues];
+      };
+
+      Get["/bands/new"] = _ => {
+        return View["band_form.cshtml"];
+      };
+
+      Post["/bands/new"] = _ => {
+        Band newBand = new Band(Request.Form["band-name"]);
+        newBand.Save();
+        return View["success.cshtml"];
       };
 
     }
